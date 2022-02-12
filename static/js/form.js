095659634c282fcbef37e13651597302b93fb5bf
Viliam Geffert <60324080+vgeffer@users.noreply.github.com>
2022-02-12 22:25:36 +0100

    Optimized file loading in form.js

--- static/js/form.js
+++ static/js/form.js
@@ -1,35 +1,63 @@
+$ = (element) => { return document.getElementById(element); };
+
 
 //Build datalists from these jsons
 const suggestions_datasets = ["/data/countries.json",
-                              "/data/marketing_types.json"];
+                              "/data/marketing_types.json",
+                              "/data/skills.json"];
 
 
 window.onload = async function() {
 
-    for (dataset of suggestions_datasets) {
+    var fetches = [];
+   
+   
+    fetches.push(fetch("/api/user-info", {method: 'POST', credentials: 'same-origin'}).then(
+        async (response) => {
+            const userinfo = await response.json(); 
+            window.userinfo = userinfo.data;
+        })
+    );
 
+    for (const dataset of suggestions_datasets) {
 
         //Load the dataset
-        const dataset_res = await fetch(dataset, {method: 'GET'});
-        const suggestions = await dataset_res.json();
+        fetches.push(fetch(dataset, {method: 'GET'}).then( 
+            async (response) => {
+                const suggestions = await response.json();
 
-        var parent = document.createElement("datalist");
-        parent.id = suggestions.name;
-
-        for (item of suggestions.data) {
-            
-            var option = document.createElement("option");
-            option.value = item;
-            option.textContent = item;
-
-            parent.appendChild(option);
-        }
-
-        document.getElementById("datasets").appendChild(parent);
+                var parent = document.createElement("datalist");
+                parent.id = suggestions.name;
+    
+                for (const item of suggestions.data) {
+    
+                    var option = document.createElement("option");
+                    option.value = item;
+                    option.textContent = item;
+    
+                    parent.appendChild(option);
+                }
+                $("datasets").appendChild(parent);
+            })
+        );
     }
 
-    const userinfo_res = await fetch("/api/user-info", {method: 'POST', credentials: 'same-origin'});
-    const userinfo = await userinfo_res.json();
-    window.userinfo = userinfo.data;
-    console.log(window.userinfo);
+    //Resolve all promises
+    try {
+        await Promise.all(fetches);
+    } catch (err) {
+
+    }
+
+    //Autofill boxes with info form MyMLH
+    $("firstname").value = window.userinfo.first_name;
+    $("lastname").value = window.userinfo.last_name;
+    
+    $("email").value = window.userinfo.email;
+    $("phone").value = window.userinfo.phone_number;
+    $("birth").value = window.userinfo.date_of_birth;
+}
+
+async function sac_callback(progress) {
+
 }
