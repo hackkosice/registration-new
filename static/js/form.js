506ba5b682578a203c33e46fdf53f16862bd6665
Viliam Geffert <60324080+vgeffer@users.noreply.github.com>
2022-02-12 15:55:28 +0100

    Added input suggestions and input suggestion loader

---
+++ static/js/form.js
@@ -0,0 +1,30 @@
+
+//Build datalists from these jsons
+const suggestions_datasets = ["/data/countries.json",
+                              "/data/marketing_types.json"];
+
+
+window.onload = async function() {
+
+    for (dataset of suggestions_datasets) {
+
+
+        //Load the dataset
+        const dataset_res = await fetch(dataset, {method: 'GET'});
+        const suggestions = await dataset_res.json();
+
+        var parent = document.createElement("datalist");
+        parent.id = suggestions.name;
+
+        for (item of suggestions.data) {
+            
+            var option = document.createElement("option");
+            option.value = item;
+            option.textContent = item;
+
+            parent.appendChild(option);
+        }
+
+       document.getElementById("datasets").appendChild(parent);
+    }
+}
