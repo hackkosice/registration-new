43e01e89b695c22ab6c72218441a8fe22b7d95ec
Viliam Geffert <viliam@hackkosice.com>
2022-02-19 19:49:51 +0100

    Updated teams api

--- services/apis/teams.js
+++ services/apis/teams.js
@@ -1,4 +1,5 @@
 const jwt           = require("jsonwebtoken");
+const crypto        = require("crypto");
 
 function error(res, status, msg) {
     return res.status(status).send({
@@ -12,22 +13,38 @@ function error(res, status, msg) {
 
 module.exports = class TeamsApiEndpoints {
 
-    constructor(database, jwt_key) {
+    constructor(database, jwt_key, mymlh_api) {
 
         this.#db = database;
         this.#jwt_key = jwt_key;
+        this.#mymlh = mymlh_api;
     }
 
     async team_create_endpoint(req, res) {
-        
-        var verification = null;
         try {
-            verification = await jwt.verify(req.cookies['verification'], this.#jwt_key);
-        } catch (err) {
-            return error(res, 401, "Authentification needed! Error: " + err);
+            const team = await this.#db.get("SELECT `team_id` FROM teams WHERE `team_name`=?;", [req.body.team_name]);
+            
+            if (team[0] !== 'undefined')
+                return error(res, 409, "Team Exists!");
+            
+            var team_code;
+            while (true) {
+                team_code = crypto.randomBytes(8).toString("base64");
+    
+                const result = await this.#db.get("SELECT `team_id` FROM applicaitons WHERE `team_code`=?;", [team_code]);
+                if (typeof result[0] === 'undefined')
+                    break; //Team does not exist, return the code
+            }
+
+            
+            await this.#db.insert("INSERT INTO teams (team_code, team_name, owner) VALUES (?, ?, ?)", [team_code, req.body.team_name, verification.uid]);
+
+            const team_id = await this.#db.get("SELECT `team_id` FROM teams WHERE `team_code=?;", [team_code])
+            await this.#db.insert("UPDATE applications SET `team_id`=? WHERE `mymlh_uid`=?;", [team_id[0].team_id, verification.uid]);
+
+        } catch(err) {
+            return error(res, 500, "Unable to join team! Error: " + err);
         }
-
-
 
     }
 
@@ -89,7 +106,6 @@ module.exports = class TeamsApiEndpoints {
             await this.#db.insert("UPDATE applications SET `team_id`=? WHERE `mymlh_uid`=?;", [null, verification.uid]);
 
 
-
             const owner = await this.#db.get("SELECT `owner` FROM teams WHERE `team_id`=?;", [req.body.team_id]);
             
             //Check if user is the owner
@@ -115,65 +131,24 @@ module.exports = class TeamsApiEndpoints {
         try {
             verification = await jwt.verify(req.cookies['verification'], this.#jwt_key);
         } catch (err) {
-            return res.status(401).send({
-                status: 'error',
-                error: {
-                    code: 401,
-                    message: "Authentification needed! Error: " + err
-                }
-            });
+            return error(res, 401,"Authentification needed! Error: " + err);
         }
 
         try {
-            const team_id = await this.#db.get("SELECT `team_id`, `owner` FROM teams WHERE `team_code`=?;", [req.body.team_id]);
-            
-                //Check if the team exist
-                if (typeof team_id[0] === 'undefined')
-                    return res.status(409).send({
-                        status: 'error',
-                        error: {
-                            code: 409,
-                            message: "Unable to join team! Error: Team does not exist!"
-                        }
-                    });
 
-            const user = await this.#db.get("SELECT `team_id`. `application_progress` FROM applications WHERE `mymlh_uid`=?;", [verification.uid]);
-                
-                //Check if your application is finnished 
-                if (typeof user[0] === 'undefined' || user[0].application_progress < 5)
-                    return res.status(409).send({
-                        status: 'error',
-                        error: {
-                            code: 409,
-                            message: "Unable to join team! Error: Application not yet finnished or not existant!"
-                        }
-                    });
-                
-                //Check if you aren't in a team already
-                if (user[0].team_id !== null)
-                    return res.status(409).send({
-                        status: 'error',
-                        error: {
-                            code: 409,
-                            message: "Unable to join team! Error: User has already team assigned!"
-                        }
-                    });
+            const owner = await this.#db.get("SELECT `owner` FROM teams WHERE `team_id`=?;", [req.body.team_id]);
 
-            const team_members = await this.#db.get("SELECT `mymlh_id` FROM applications WHERE `team_id`=?;", [team_id[0].team_id]);
-                
-                //Check if team isn't full            
-                if (team_members.length === 4)
-                    return res.status(409).send({
-                        status: 'error',
-                        error: {
-                            code: 409,
-                            message: "Unable to join team! Error: Team is full!"
-                        }
-                    });
+            if (typeof owner[0] === 'undefined')
+                return error(res, 400, "Selected team does not exist!");
 
-            //Add user to the team
-            const update_result = await this.#db.insert("UPDATE applications SET `team_id`=? WHERE `mymlh_uid`=?;", [team_id[0].team_id, verification.uid]);
-            console.log(update_result);
+            if (verification.uid !== owner[0].owner)
+                return error(res, 400, "Error: User is not the owner of the team!");
+
+            if (req.body.target === owner[0].owner)
+                return error(res, 400, "Error: Owner can't kick self!");
+
+            //Remove user from the team
+            await this.#db.insert("UPDATE applications SET `team_id`=? WHERE `mymlh_uid`=?;", [null, req.body.target]);
             
 
         } catch(err) {
@@ -182,10 +157,39 @@ module.exports = class TeamsApiEndpoints {
     }
 
     async team_info_endpoint(req, res) {
+        var verification = null;
+        try {
+            verification = await jwt.verify(req.cookies['verification'], this.#jwt_key);
+        } catch (err) {
+            return error(res, 401,"Authentification needed! Error: " + err);
+        }
+
+        const users = await this.#mymlh.get_all_users();
+        const team_id = await this.#db.get("SELECT `team_id` FROM applications WHERE `mymlh_uid`=?;", [verification.uid]); 
+
+        if (team_id !== null)
+            return res.status(200).send([]);
+
+        const team_members = await this.#db.get("SELECT `mymlh_uid` FROM applications WHERE `team_id`=?")
+
+        var result = [];
+
+        for (const user of users.data) {
+            for (var i = 0; i < 4; i++)
+                if(user.id === team_members[i].mymlh_uid) {
+                    result.push({
+                        mymlh_uid: team_members[i].mymlh_uid,
+                        name: (user.first_name + user.last_name)
+                    });
+                    break;
+                }
+        }
+
+        res.status(200).send(result);
 
     }
 
     #db = null;
     #jwt_key = null;
-
+    #mymlh = null;
 }
